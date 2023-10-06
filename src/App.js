78df2a37e302836a206145f70867d96e44b9a359
binoyam <binyam.techan01@gmail.com>
2023-10-06 05:22:31 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import Data from './components/data.json'
-
+import React from "react";
+import "./index.css";
+import Data from "./components/data.json";
+import Header from "./components/Header";
 function App() {
+  const data = Data;
 
   return (
-   <header></header>
-  )
+    <main className="app">
+      <div className="hero">
+      <Header />
+      <section className="chart"></section>
+      <section className="bottom"></section>
+
+      </div>
+     
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Logo from '../images/logo.svg'
+function Header() {
+  return (
+    <section className="top">
+        <div className="text">
+      <h3>My balance</h3>
+      <p>$921.48</p>
+        </div>
+        <div className="logo-div">
+            <img src={Logo} alt="Logo" />
+        </div>
+    </section>
+  );
+}
+
+export default Header;
