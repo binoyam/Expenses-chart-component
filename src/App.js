985adac7c3850d3ac7ff59eef244b858af5b1662
binoyam <binyam.techan01@gmail.com>
2023-10-06 05:44:14 +0300

    layout

--- src/App.js
+++ src/App.js
@@ -8,12 +8,33 @@ function App() {
   return (
     <main className="app">
       <div className="hero">
-      <Header />
-      <section className="chart"></section>
-      <section className="bottom"></section>
-
+        <Header />
+        <section className="expense">
+          <h2>Spending - Last & days</h2>
+          <div className="chart">
+            <div className="mon">mon</div>
+            <div className="tue">tue</div>
+            <div className="wed">wed</div>
+            <div className="thu">thu</div>
+            <div className="fri">fri</div>
+            <div className="sat">sat</div>
+            <div className="sun">sun</div>
+          </div>
+          <div className="border"></div>
+          <div className="footer">
+            <p className="this">Total this month</p>
+            <div className="total-div">
+              <div className="total-amount">
+                <h1>$437.53</h1>
+              </div>
+              <div className="note">
+                <p className="status">+2.4%</p>
+                <p className="this">from last month</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-     
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from "react";
-import Logo from '../images/logo.svg'
+import Logo from "../images/logo.svg";
 function Header() {
   return (
     <section className="top">
-        <div className="text">
-      <h3>My balance</h3>
-      <p>$921.48</p>
-        </div>
-        <div className="logo-div">
-            <img src={Logo} alt="Logo" />
-        </div>
+      <div className="text">
+        <h3>My balance</h3>
+        <p>$921.48</p>
+      </div>
+      <div className="logo-div">
+        <img src={Logo} alt="Logo" />
+      </div>
     </section>
   );
 }
